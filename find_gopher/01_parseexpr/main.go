37ccf4fed3e08de3b445c3905b5dea1be2020427
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-06-08 21:52:55 +0900

    Initial commit

---
+++ find_gopher/01_parseexpr/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"log"
+)
+
+func main() {
+	// 式をパースする
+	expr, err := parser.ParseExpr("v + 1")
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	// v + 1の抽象構文木(AST)を表示する
+	ast.Print(nil, expr)
+	fmt.Println(expr)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tetsuzawa/static_analysis
+
+go 1.13
